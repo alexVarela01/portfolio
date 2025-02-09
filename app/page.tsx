c7455b4ee25acb7941ee165a559e1ebceeb4846b
Alexandre <nawtz_sky@hotmail.com>
2025-02-09 02:19:33 +0000

    fixed build issues

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
         Alexandre <span>Varela</span>
       </span>
 
-      <hr />
+      <hr/>
       
-      <div><span>//</span> Coming soon!</div> 
+      <div><span>{"//"}</span> Coming soon!</div> 
 
       <div className="tag">
         <span className="purple">let</span>
         <span className="white">status</span>
         <span className="purple">=</span>
-        <span className="green">"in development"</span>
+        <span className="green">&quot;in development&quot;</span>
       </div>
     </div>
   );
