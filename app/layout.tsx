0d2ad3d6cf580bb9b6a3179624686886116b8714
Alexandre <nawtz_sky@hotmail.com>
2025-02-09 02:16:46 +0000

    coming soon portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "./global.css";
+
+export const metadata = {
+  title: "Varela | Portfolio",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import "./page.css";
+
+export default function Home() {
+  return (
+    <div className='coming'>
+      <span>
+        Alexandre <span>Varela</span>
+      </span>
+
+      <hr />
+      
+      <div><span>//</span> Coming soon!</div> 
+
+      <div className="tag">
+        <span className="purple">let</span>
+        <span className="white">status</span>
+        <span className="purple">=</span>
+        <span className="green">"in development"</span>
+      </div>
+    </div>
+  );
+}
+
