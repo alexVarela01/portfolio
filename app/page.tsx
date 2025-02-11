ec979f9aee92475ec5292b9918e484d30b7632e1
Alexandre <nawtz_sky@hotmail.com>
2025-02-11 13:46:35 +0000

    main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,31 @@
 'use client';
 import "./page.css";
+import { BiTestTube } from "react-icons/bi";
+import CodePanel from "./Components/CodePanel/CodePanel";
 
 export default function Home() {
   return (
-    <div className='coming'>
-      <span>
-        Alexandre <span>Varela</span>
-      </span>
+    <div className='container'>
+      <div className="main">
+        <div className="info">
+          <div className="left">
+            <div className="badge"><BiTestTube/>Full Stack and QA Engineer</div>
 
-      <hr/>
-      
-      <div><span>{"//"}</span> Coming soon!</div> 
-
-      <div className="tag">
-        <span className="purple">let</span>
-        <span className="white">status</span>
-        <span className="purple">=</span>
-        <span className="green">&quot;in development&quot;</span>
+            <p className="comment">Initialize developer</p>
+            <h1 className="name code">
+              <span className="purple">Alexandre</span>
+              <span className="blue">Varela</span>
+            </h1>
+            
+            <p className="code role">{"<"}<span className="red">AnAwesomeGuy</span>{"/>"}</p>
+            
+            <p className="description">Building elegant solutions to complex problems with clean, maintainable code. Specialized in full-stack development and quality assurance, both manual and automated testing.</p>
+          </div>
+          
+          <div className="right">
+            <CodePanel/>
+          </div>
+        </div>
       </div>
     </div>
   );
