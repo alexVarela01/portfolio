a73076107ee46d36f7363adeda750c67bf9f6c7c
Alexandre <nawtz_sky@hotmail.com>
2025-02-19 12:16:58 +0000

    added canonical url and crawl issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,17 @@ import "./global.css";
 
 export const metadata = {
   title: "Varela | Portfolio",
+  description: "My personal portfolio site",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <head>
+        <link rel="canonical" href="https://eshkay.dev" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
